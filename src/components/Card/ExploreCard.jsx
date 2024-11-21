bf49b21ce3e7c229e30c915e4395178eed221124
mahamudul hasan seber <sebermh@gmail.com>
2024-11-21 14:48:37 +0600

    AOS-css add Animation Style

--- src/components/Card/ExploreCard.jsx
+++ src/components/Card/ExploreCard.jsx
@@ -1,5 +1,12 @@
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const ExploreCard = ({ explore }) => {
+    useEffect(() => {
+        Aos.init({ duration: 1000, once: true })
+    }, []);
+
     const { image,
         categoryName,
         shortDescription,
@@ -12,17 +19,25 @@ const ExploreCard = ({ explore }) => {
         specialInstructions } = explore
     return (
         <div className="bg-slate-100 p-6">
-            <div className="card bg-base-100 lg:w-[600px] shadow-xl">
-                <figure className="px-10 pt-10">
+            <div
+                data-aos="fade-up"
+                data-aos-delay="100"
+                className="card bg-base-100 lg:w-[600px] shadow-xl">
+                <figure
+                    data-aos="fade-up"
+                    data-aos-delay="100"
+                    className="px-10 pt-10">
                     <img
+                        data-aos="zoom-in"
+                        data-aos-delay="200"
                         src={image}
                         alt="travel"
                         className="rounded-xl h-[276px] object-cover" />
                 </figure>
                 <div className="card-body items-center text-center">
-                    <h2 className="card-title text-2xl font-extrabold text-green-900">{categoryName}</h2>
-                    <p className="font-semibold text-gray-500"><i>{shortDescription}</i></p>
-                    <ul className="text-start text-green-600">
+                    <h2 className="card-title text-2xl font-extrabold text-green-900" data-aos="fade-up">{categoryName}</h2>
+                    <p className="font-semibold text-gray-500" data-aos="slide-right"><i>{shortDescription}</i></p>
+                    <ul className="text-start text-green-600" data-aos="slide-left">
                         <li>Cost: {adventureCost}</li>
                         <li>Map: {location}</li>
                         <li>Time: {duration}</li>
@@ -31,13 +46,13 @@ const ExploreCard = ({ explore }) => {
                     </ul>
                     <p className="border border-dashed  border-gray-500 w-[400px]"></p>
                     <div className="flex gap-20 pt-5">
-                        <ul className="font-bold text-gray-400 ">
+                        <ul className="font-bold text-gray-400" data-aos="slide-left">
                             <span className="text-black text-xl font-semibold">includedItems !</span>
                             {includedItems.map((feature, index) => (
                                 <li key={index}>{feature}</li>
                             ))}
                         </ul>
-                        <ul className="font-bold text-gray-400">
+                        <ul className="font-bold text-gray-400" data-aos="slide-left">
                             <span className="text-black text-xl font-semibold">specialInstructions !</span>
                             {specialInstructions.map((feature, index) => (
                                 <li key={index}>{feature}</li>
--- src/components/Card/TravelCard.jsx
+++ src/components/Card/TravelCard.jsx
@@ -1,3 +1,6 @@
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 import { MdTravelExplore } from "react-icons/md";
 import { Link, } from "react-router-dom";
 
@@ -5,20 +8,29 @@ import { Link, } from "react-router-dom";
 const TravelCard = ({ travel }) => {
     const { title, image, ecoFriendlyFeatures } = travel;
 
-
+    useEffect(() => {
+        Aos.init({ duration: 1000, once: true })
+    }, []);
 
 
     return (
-        <div className="card bg-base-100 w-96 shadow-xl shadow-green-700">
-            <figure className="">
+        <div
+            data-aos="fade-up"
+            data-aos-delay="100"
+            className="card bg-base-100 w-96 shadow-xl shadow-green-700">
+            <figure
+                data-aos="fade-up"
+                data-aos-delay="100">
                 <img
+                    data-aos="zoom-in"
+                    data-aos-delay="200"
                     className=" p-4 rounded-3xl w-full h-[276px] object-cover"
                     src={image}
                     alt="Travel" />
             </figure>
             <div className="card-body">
-                <h2 className="card-title text-green-800 text-2xl font-bold">{title}</h2>
-                <ul className="font-bold text-gray-400">
+                <h2 className="card-title text-green-800 text-2xl font-bold " data-aos="fade-up">{title}</h2>
+                <ul className="font-bold text-gray-400" data-aos="slide-left">
                     {ecoFriendlyFeatures.map((feature, index) => (
                         <li key={index}>{feature}</li>
                     ))}
@@ -26,6 +38,8 @@ const TravelCard = ({ travel }) => {
 
                 <div className="  card-actions justify-end">
                     <Link to="exploreNow"
+                        data-aos="fade-up"
+                        data-aos-delay="300"
                         className=" btn w-42 bg-gradient-to-r from-green-600 to-green-900 text-white font-semibold ">Explore Now <MdTravelExplore /></Link>
                 </div>
 
